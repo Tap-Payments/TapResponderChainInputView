6ae10d3d776e7e9a5a643a83b4718e442410615f
O Rabie <o.rabie@tap.company>
2020-09-22 13:55:03 +0200

    Upgrade syntax to match the compiler in Xcode 12+

---
+++ TapResponderChainInputViewV2/TapResponderChainInputViewV2/TapResponderChainInputViewV2.h
@@ -0,0 +1,18 @@
+//
+//  TapResponderChainInputViewV2.h
+//  TapResponderChainInputViewV2
+//
+//  Created by Osama Rabie on 9/22/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TapResponderChainInputViewV2.
+FOUNDATION_EXPORT double TapResponderChainInputViewV2VersionNumber;
+
+//! Project version string for TapResponderChainInputViewV2.
+FOUNDATION_EXPORT const unsigned char TapResponderChainInputViewV2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TapResponderChainInputViewV2/PublicHeader.h>
+
+
